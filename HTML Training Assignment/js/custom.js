c9e3b166a77b1c81140ae8d5a2255953a564ce06
learndev0509 <learndev0509@gmail.com>
2023-01-31 17:59:28 +0530

    Assignment-4

--- HTML Training Assignment/js/custom.js
+++ HTML Training Assignment/js/custom.js
@@ -1,4 +1,38 @@
-$('.nav-link').on('click', function() {
+$('.nav-link').on('click', function () {
     $('.active-link').removeClass('active-link');
     $(this).addClass('active-link');
-    });
+});
+
+
+$('.toggalbtn').click(function () {
+
+    $(this).toggleClass("click");
+    if ($('#sidebarMenu').hasClass('xyz')) {
+        $('#sidebarMenu').removeClass("d-none xyz");
+        $('#sidebarMenu').addClass("d-none d-lg-block d-xl-block d-xxl-block");
+        $('#mainContent').removeClass("col-lg-12");
+        $('#mainContent').addClass("col-lg-10");
+    } else {
+        $('#sidebarMenu').removeClass("d-none d-lg-block d-xl-block d-xxl-block");
+        $('#sidebarMenu').addClass("d-none xyz");
+        $('#mainContent').removeClass("col-lg-10");
+        $('#mainContent').addClass("col-lg-12");
+    }
+    if ($(window).width() < 570) {
+        
+
+        if ($('#sidebarMenu').hasClass('xyz')) {
+            $('#sidebarxyz').removeClass("d-none d-lg-block d-xl-block d-xxl-block");
+            $('#sidebarMenu').removeClass("d-none d-lg-block d-xl-block d-xxl-block");
+            $('#sidebarMenu').addClass("show d-sm-block xyz");
+        } 
+        else {
+            
+            $('#sidebarxyz').addClass("d-none d-lg-block d-xl-block d-xxl-block");
+            $('#sidebarMenu').addClass("d-none d-lg-block d-xl-block d-xxl-block");
+            $('#sidebarMenu').removeClass("show d-sm-block xyz");
+        }
+      
+        
+    }
+});
